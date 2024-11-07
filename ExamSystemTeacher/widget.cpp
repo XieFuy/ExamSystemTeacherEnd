79731e0826606654571d0ae2509487c690f02ad6
Woody <2496802850@qq.com>
2024-11-07 22:58:10 +0800

    1、完成登录页面前端的数据校验功能和登录校验功能。

--- ExamSystemTeacher/widget.cpp
+++ ExamSystemTeacher/widget.cpp
@@ -9,6 +9,57 @@ Widget::Widget(QWidget *parent)
     this->setWindowTitle("在线考试系统-教师端");
     this->setWindowIcon(QIcon(":/icons/winIcon.png"));
     this->setContentsMargins(0,0,0,0);//设置布局无边界
+    this->ui->label_7->hide();
+    this->ui->label_8->hide();
+    this->m_isAcountOk = false;
+    this->m_isPasswordOk = false;
+    QRegularExpression regex("^[\\d]+(\\.\\d+)?$");
+    QRegularExpression regex2("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$");
+    this->reAacount = regex;
+    this->rePassword = regex2;
+    QObject::connect(this->ui->lineEdit,&QLineEdit::textChanged,this,&Widget::onAccountChange);
+    QObject::connect(this->ui->lineEdit_2,&QLineEdit::textChanged,this,&Widget::onPasswordChange);
+    QObject::connect(this->ui->pushButton,&QPushButton::clicked,this,&Widget::login);
+}
+
+void Widget::login() //执行登录逻辑
+{
+    if(this->ui->lineEdit->text().size() == 0 || this->ui->lineEdit_2->text().size() == 0 )
+    {
+        QMessageBox* box = new QMessageBox(QMessageBox::Warning,"错误提示","职工号或密码输入不能为空！",QMessageBox::Ok);
+        box->exec();
+        delete box;
+    }
+    else if((!this->m_isAcountOk) || (!this->m_isPasswordOk))
+    {
+        QMessageBox* box = new QMessageBox(QMessageBox::Warning,"错误提示","职工号或密码输入格式不正确！",QMessageBox::Ok);
+        box->exec();
+        delete box;
+    }
+}
+
+void Widget::onAccountChange(QString str)
+{
+    QRegularExpressionMatch match = this->reAacount.match(this->ui->lineEdit->text());
+    if (match.hasMatch()) {
+        this->m_isAcountOk = true;
+        this->ui->label_7->hide(); // 输入符合正则表达式，隐藏错误标签
+    } else {
+        this->m_isAcountOk = false;
+        this->ui->label_7->show(); // 输入不符合正则表达式，显示错误标签
+    }
+}
+
+void Widget::onPasswordChange(QString str)
+{
+    QRegularExpressionMatch match = this->rePassword.match(this->ui->lineEdit_2->text());
+    if (match.hasMatch()) {
+        this->m_isPasswordOk = true;
+        this->ui->label_8->hide(); // 输入符合正则表达式，隐藏错误标签
+    } else {
+        this->m_isPasswordOk = false;
+        this->ui->label_8->show(); // 输入不符合正则表达式，显示错误标签
+    }
 }
 
 Widget::~Widget()
--- ExamSystemTeacher/widget.h
+++ ExamSystemTeacher/widget.h
@@ -3,6 +3,9 @@
 
 #include <QWidget>
 #include <QIcon>
+#include <QRegularExpressionMatch>
+#include <QMessageBox>
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
@@ -14,7 +17,16 @@ class Widget : public QWidget //该界面为教职工登录界面
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
-
+public slots:
+    void onAccountChange(QString str);//账号输入改变
+    void onPasswordChange(QString str);//密码输入改变
+private:
+    void login(); //登录函数
+private:
+    QRegularExpression reAacount;
+    QRegularExpression rePassword;
+    bool m_isAcountOk;
+    bool m_isPasswordOk;
 private:
     Ui::Widget *ui;
 };
